40a611bd0cd9fd06fe07a5d2c65fc25cbc6a4e62
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-31 11:05:24 +0100

    feat: add InventorySerializer for inventory items

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from inventory.models import InventoryItem
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "email"]
+
+        
+class InventorySerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = InventoryItem
+        fields = ["id", "name", "description", "quantity", "price", "owner"]
